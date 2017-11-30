fe14755d18d4dd721ad84bc3c9813ba4f9993459
Jonas Kello <jonas.kello@divid.se>
2017-11-30 12:25:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Repro of WDS disconnected! Infinate Loop
+
+```
+yarn install
+yarn start
+```
+
+# Versions
+
+```
+$ node --version
+v8.9.1
+
+$ yarn --version
+1.2.1
+
+Windows 10:
+Version	10.0.16299 Build 16299
+
+Chrome:
+Version 62.0.3202.94 (Official Build) (64-bit)
+```
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var fs = require("fs");
+var path = require("path");
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  devtool: "source-map",
+  entry: {
+    app: ["babel-polyfill", "./src/index.js"]
+  },
+  context: path.resolve(__dirname, "."),
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+    chunkFilename: "[name].chunk.js"
+  },
+  stats: "errors-only",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      chunks: ["app"],
+      filename: "index.html"
+    })
+  ],
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  devServer: {
+    https: true,
+    port: 8080,
+    host: "localhost",
+    stats: "errors-only",
+    inline: true,
+    historyApiFallback: true
+  }
+};
